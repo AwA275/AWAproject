9d7d754b309cce09476148bbbfe201bf07c08900
Student22_4 <Student22_4@22KAB-4>
2026-04-18 17:37:44 +0300

    Добавьте файлы проекта.

---
+++ AWAproject/DB_connect/ConnectionClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWAproject.DB_connect
+{
+    internal class ConnectionClass
+    {
+        public static BdanvarEntities db = new BdanvarEntities();
+    }
+}
---
+++ AWAproject/Pages/ListPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AWAproject.Pages;
+using AWAproject.DB_connect;
+
+namespace AWAproject.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListPage.xaml
+    /// </summary>
+    public partial class ListPage : Page
+    {
+        public ListPage()
+        {
+            //вывод списка
+            InitializeComponent();
+            Listlw.ItemsSource = DB_connect.ConnectionClass.db.Materials.ToList();
+        }
+
+        private void Btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddPage());
+
+        }
+
+        private void Btn_redact_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RedactPage());
+        }
+
+        private void Cmb_Sort_Money_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
